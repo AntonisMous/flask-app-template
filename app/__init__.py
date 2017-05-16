ad7ec669ad1ee8c164ce8c4ae86c1df3da66d84a
AntonisMous <moustant21@gmail.com>
2017-05-16 17:03:21 +0300

    design changes

--- app/__init__.py
+++ app/__init__.py
@@ -12,10 +12,11 @@ from flask.ext.login import LoginManager
 
 app = Flask(__name__)
 
-#Configuration of application, see configuration.py, choose one and uncomment.
-#app.config.from_object('configuration.ProductionConfig')
+
+# Configuration of application, see configuration.py, choose one and uncomment.
+# app.config.from_object('configuration.ProductionConfig')
 app.config.from_object('app.configuration.DevelopmentConfig')
-#app.config.from_object('configuration.TestingConfig')
+# app.config.from_object('configuration.TestingConfig')
 
 bs = Bootstrap(app) #flask-bootstrap
 db = SQLAlchemy(app) #flask-sqlalchemy
